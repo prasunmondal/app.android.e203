20031246af71f758879e5ab6e286627d410573bf
Prasun <pmandal@akamai.com>
2020-04-05 08:31:29 +0530

    cleanUp and working.. need to fix shiwing the dialogs

--- E203/app/src/main/java/com/example/e203/MainActivity.kt
+++ E203/app/src/main/java/com/example/e203/MainActivity.kt
@@ -7,61 +7,59 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : AppCompatActivity() {
 
 
-    val submitFormURL: String = "https://docs.google.com/forms/d/e/1FAIpQLSdYzijpIalsSmnyQ53tkZawzOM40yYYR92O0TPfAhSRcgo9Wg/viewform?usp=sf_link";
-    val detailsFormURL: String = "https://docs.google.com/forms/d/e/1FAIpQLSfpjgKBlK678ncJGTRV1-iwCzGuYsKXea71k7uQtJficGD7kw/viewform";
-    val enlistFormURL: String = "https://docs.google.com/forms/d/e/1FAIpQLSdoq9CzHE7t2CY85VG7MXLDSphCZhgnXli3blmOE5k-FT04mw/viewform";
-    val editPage = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRZQ28x7jpdIOzT2PA6iTCTcyTHM9tVPkv2ezuqd4LFOWu9SJqImGM7ML8ejdQB01SdjfTZnoHogzUt/pubhtml?gid=16104355&single=true";
+    val submitFormURL: String =
+        "https://docs.google.com/forms/d/e/1FAIpQLSdYzijpIalsSmnyQ53tkZawzOM40yYYR92O0TPfAhSRcgo9Wg/viewform?usp=sf_link";
+    val detailsFormURL: String =
+        "https://docs.google.com/forms/d/e/1FAIpQLSfpjgKBlK678ncJGTRV1-iwCzGuYsKXea71k7uQtJficGD7kw/viewform";
+    val enlistFormURL: String =
+        "https://docs.google.com/forms/d/e/1FAIpQLSdoq9CzHE7t2CY85VG7MXLDSphCZhgnXli3blmOE5k-FT04mw/viewform";
+    val editPage =
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vRZQ28x7jpdIOzT2PA6iTCTcyTHM9tVPkv2ezuqd4LFOWu9SJqImGM7ML8ejdQB01SdjfTZnoHogzUt/pubhtml?gid=16104355&single=true";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myWebView: WebView = findViewById(R.id.formView)
-        myWebView.webViewClient = MyWebViewClient()
-        val webSettings = myWebView.settings
+        val webView: WebView = findViewById(R.id.formView)
+        webView.webViewClient = MyWebViewClient()
+        val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
+        webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY)
+        webView.getSettings().setBuiltInZoomControls(true)
+        webView.getSettings().setUseWideViewPort(true)
+        webView.getSettings().setLoadWithOverviewMode(true)
 
-        myWebView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY)
-
-
-
-        myWebView.loadUrl(submitFormURL);
-
-        val button: FloatingActionButton = findViewById(R.id.addButton2)
-        button.bringToFront();
+        loadPage(webView, submitFormURL)
     }
 
-    fun loadPage(view: WebView, url: String) {
-//        val myWebView: WebView = findViewById(R.id.formView)
-
-        view.getSettings().setBuiltInZoomControls(true)
-        view.getSettings().setUseWideViewPort(true)
-        view.getSettings().setLoadWithOverviewMode(true)
-
+    fun loadPage(webView: WebView, url: String) {
         var progressDialog = ProgressDialog(this)
         progressDialog!!.setMessage("Loading...")
         progressDialog!!.show()
 
-        view.setWebViewClient(object : WebViewClient() {
+        webView.setWebViewClient(object : WebViewClient() {
             override fun onPageFinished(view: WebView, url: String) {
                 if (progressDialog!!.isShowing()) {
                     progressDialog!!.dismiss()
                 }
             }
 
-            override fun onReceivedError(view: WebView, errorCode: Int, description: String, failingUrl: String) {
+            override fun onReceivedError(
+                view: WebView,
+                errorCode: Int,
+                description: String,
+                failingUrl: String
+            ) {
                 Toast.makeText(this@MainActivity, "Error:$description", Toast.LENGTH_SHORT).show()
-
             }
         })
 
-        view.loadUrl(url)
+        webView.loadUrl(url)
     }
 
     fun onClickOnceMoreAdd(view: View) {
