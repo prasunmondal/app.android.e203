64a87cbed021b8f907001276d69ccf722a86c347
Prasun <pmandal@akamai.com>
2020-04-11 13:04:16 +0530

    downloads and updates onCreate

--- E203/app/src/main/java/com/example/e203/MainActivity.kt
+++ E203/app/src/main/java/com/example/e203/MainActivity.kt
@@ -17,6 +17,9 @@ import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.example.e203.Utils.*
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -50,6 +53,13 @@ class MainActivity : AppCompatActivity() {
         haveStoragePermission()
 
         loadPage(webView, submitFormURL)
+        val apkUrl = apklink
+        downloadController = DownloadController(this, apkUrl)
+
+//        buttonDownload.setOnClickListener {
+            // check storage permission granted if yes then start downloading file
+            checkStoragePermission()
+//        }
     }
 
     fun loadPage(webView: WebView, url: String) {
@@ -133,6 +143,65 @@ class MainActivity : AppCompatActivity() {
     fun onClickEnlist(view: View) {
         val myWebView: WebView = findViewById(R.id.formView)
         loadPage(myWebView, editPage);
+    }
+
+    companion object {
+        const val PERMISSION_REQUEST_STORAGE = 0
+    }
+
+    lateinit var downloadController: DownloadController
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_REQUEST_STORAGE) {
+            // Request for camera permission.
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // start downloading
+                downloadController.enqueueDownload()
+            } else {
+                // Permission request was denied.
+                mainLayout.showSnackbar(R.string.storage_permission_denied, Snackbar.LENGTH_SHORT)
+            }
+        }
+    }
+
+
+    private fun checkStoragePermission() {
+        // Check if the storage permission has been granted
+        if (checkSelfPermissionCompat(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            // start downloading
+            downloadController.enqueueDownload()
+            Log.d("Download: ","started")
+        } else {
+            // Permission is missing and must be requested.
+            requestStoragePermission()
+        }
+    }
+
+    private fun requestStoragePermission() {
+
+        if (shouldShowRequestPermissionRationaleCompat(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            mainLayout.showSnackbar(
+                R.string.storage_access_required,
+                Snackbar.LENGTH_INDEFINITE, R.string.ok
+            ) {
+                requestPermissionsCompat(
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    PERMISSION_REQUEST_STORAGE
+                )
+            }
+
+        } else {
+            requestPermissionsCompat(
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                PERMISSION_REQUEST_STORAGE
+            )
+        }
     }
 }
 
