49232e335c412499ff3325391a6dec15a8c6be76
Prasun <pmandal@akamai.com>
2020-04-12 22:49:04 +0530

    comment out unnecessary code

--- E203/app/src/main/java/com/example/e203/Utils/Permissions.kt
+++ E203/app/src/main/java/com/example/e203/Utils/Permissions.kt
@@ -1,47 +1,47 @@
 package com.example.e203
 
-import android.Manifest
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
-import com.example.e203.Utils.*
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.*
-
-fun storagePermission() {
-    val permissions = Permissions();
-    permissions.storagePermission()
-}
-
-class Permissions : AppCompatActivity() {
-
-    fun storagePermission() {
-        checkStoragePermission()
-    }
-
-    private fun checkStoragePermission() {
-        // Check if the storage permission has been granted
-        if (checkSelfPermissionCompat(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-            requestStoragePermission()
-    }
-
-    private fun requestStoragePermission() {
-        val PERMISSION_REQUEST_STORAGE = 0
-        if (shouldShowRequestPermissionRationaleCompat(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            mainLayout.showSnackbar(
-                R.string.storage_access_required,
-                Snackbar.LENGTH_INDEFINITE, R.string.ok
-            ) {
-                requestPermissionsCompat(
-                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    PERMISSION_REQUEST_STORAGE
-                )
-            }
-
-        } else {
-            requestPermissionsCompat(
-                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                PERMISSION_REQUEST_STORAGE
-            )
-        }
-    }
-}
+//import android.Manifest
+//import android.content.pm.PackageManager
+//import androidx.appcompat.app.AppCompatActivity
+//import com.example.e203.Utils.*
+//import com.google.android.material.snackbar.Snackbar
+//import kotlinx.android.synthetic.main.activity_main.*
+//
+//fun storagePermission() {
+//    val permissions = Permissions();
+//    permissions.storagePermission()
+//}
+//
+//class Permissions : AppCompatActivity() {
+//
+//    fun storagePermission() {
+//        checkStoragePermission()
+//    }
+//
+//    private fun checkStoragePermission() {
+//        // Check if the storage permission has been granted
+//        if (checkSelfPermissionCompat(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+//            requestStoragePermission()
+//    }
+//
+//    private fun requestStoragePermission() {
+//        val PERMISSION_REQUEST_STORAGE = 0
+//        if (shouldShowRequestPermissionRationaleCompat(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+//            mainLayout.showSnackbar(
+//                R.string.storage_access_required,
+//                Snackbar.LENGTH_INDEFINITE, R.string.ok
+//            ) {
+//                requestPermissionsCompat(
+//                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+//                    PERMISSION_REQUEST_STORAGE
+//                )
+//            }
+//
+//        } else {
+//            requestPermissionsCompat(
+//                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+//                PERMISSION_REQUEST_STORAGE
+//            )
+//        }
+//    }
+//}
