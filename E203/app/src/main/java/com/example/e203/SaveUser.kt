db71ebea2c07a243d96e6f3152d9e678e5e23bea
Prasun <pmandal@akamai.com>
2020-04-24 12:14:28 +0530

    organize - working good

--- E203/app/src/main/java/com/example/e203/SaveUser.kt
+++ E203/app/src/main/java/com/example/e203/SaveUser.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import com.example.e203.Utils.showSnackbar
 
 class SaveUser : AppCompatActivity() {
 
@@ -14,15 +16,30 @@ class SaveUser : AppCompatActivity() {
         setContentView(R.layout.activity_save_user2)
     }
 
-    fun saveUser(view: View) {
+    fun onClickSaveUsername(view: View) {
         val myWebView: EditText = findViewById(com.example.e203.R.id.userNameSelection)
-        Log.d("Username: ", myWebView.getText().toString())
-        goToMainPage()
+        val username = myWebView.getText().toString()
+        Log.d("Username: ", username)
+
+        isValidUserName(username)
+        var writeSuccessful = writeUsernameToFile(username)
+        Log.d("WriteStatus_Username: ", writeSuccessful.toString())
+
+        if(writeSuccessful) goToMainPage()
+        else Toast.makeText(this@SaveUser, "Error: Please Enter a Valid Name!", Toast.LENGTH_SHORT).show()
     }
 
     fun goToMainPage() {
         val i = Intent(this@SaveUser, MainActivity::class.java)
         startActivity(i)
         finish()
+    }
+
+    fun writeUsernameToFile(username: String): Boolean {
+        return false
+    }
+
+    fun isValidUserName(username: String): Boolean {
+        return true
     }
 }
