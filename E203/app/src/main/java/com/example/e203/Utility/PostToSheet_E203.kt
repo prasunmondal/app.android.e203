06f62b18ceab3a304d4922d73ed8d5211b204806
Prasun <pmandal@akamai.com>
2020-06-15 23:15:12 +0530

    sort logs by username

--- E203/app/src/main/java/com/example/e203/Utility/PostToSheet_E203.kt
+++ E203/app/src/main/java/com/example/e203/Utility/PostToSheet_E203.kt
@@ -21,7 +21,7 @@ class PostToSheet_E203 {
             var username = LocalConfig.Singleton.instance.getValue(LocalConfig.Singleton.instance.USERNAME)!!
 
             PostToSheet().post(context, HardData.Singleton.instance.GSheet_logs_PostScript,
-                HardData.Singleton.instance.GSheet_User_logs_spreadsheetURL, HardData.Singleton.instance.GSheet_User_logs_sheetName,
+                HardData.Singleton.instance.GSheet_User_logs_spreadsheetURL, username,
             listOf(appVersion, username, deviceID, text))
         } catch (e: Exception) {
 
