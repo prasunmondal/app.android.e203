3bf6a1cf905a8599d224c47fb65438f7e2b1dadc
Prasun <pmandal@akamai.com>
2020-06-17 20:49:49 +0530

    create tabs by device id

--- E203/app/src/main/java/com/example/e203/Utility/PostToSheet_E203.kt
+++ E203/app/src/main/java/com/example/e203/Utility/PostToSheet_E203.kt
@@ -21,7 +21,7 @@ class PostToSheet_E203 {
             var username = LocalConfig.Singleton.instance.getValue(LocalConfig.Singleton.instance.USERNAME)!!
 
             PostToSheet().post(context, HardData.Singleton.instance.GSheet_logs_PostScript,
-                HardData.Singleton.instance.GSheet_User_logs_spreadsheetURL, username,
+                HardData.Singleton.instance.GSheet_User_logs_spreadsheetURL, deviceID,
             listOf(appVersion, username, deviceID, text))
         } catch (e: Exception) {
 
